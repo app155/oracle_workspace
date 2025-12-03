d2e7aa9c1ef0865c30a370104c3a9f62a9386066
1k10 <77734986+app155@users.noreply.github.com>
2025-12-03 18:16:09 +0900

    20251203
    20251203

---
+++ bbs.sql
@@ -0,0 +1,18 @@
+select * from tab;
+show user;
+
+create table visit (
+no          number(5) not null,
+writer      varchar2(20) not null,
+memo        varchar2(4000) not null,
+regdate     date not null,
+constraint VISIT_PK primary key(no) );
+
+create sequence visit_seq
+start with 1 
+increment by 1 
+nomaxvalue 
+nocycle 
+nocache;
+
+commit;
